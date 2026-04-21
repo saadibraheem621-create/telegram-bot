98176e19916b672cb4d61333315913c91eb56e39
saadibraheem621-create <saadibraheem621@gmail.com>
2026-04-22 00:05:48 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import requests
+
+TOKEN = "YOUR_BOT_TOKEN"
+CHAT_ID = "YOUR_CHAT_ID"
+
+def send_message(text):
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    data = {"chat_id": CHAT_ID, "text": text}
+    requests.post(url, data=data)
+
+send_message("البوت شغال 🔥")
